5da1de23d548db8b1a08174871691d816f816e4a
Or Levy <olevy111@gmail.com>
2024-11-06 15:22:07 +0200

    Update Bill3.java

--- Bill3.java
+++ Bill3.java
@@ -6,7 +6,7 @@ public class Bill3 {
 		String name2 = args[1];
 		String name3 = args[2];
 		int num = Integer.parseInt(args[3]);
-	    double sum = (double) num / 3;
+	    double sum = num / 3.0;
 		double result1 = Math.ceil(sum);
 		
 		System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 + ": pay " + result1 + " Shekels each.");
