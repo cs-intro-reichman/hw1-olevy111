6e88c23003ead54a6441d1d0973b293886b25e28
Or Levy <olevy111@gmail.com>
2024-11-07 09:17:55 +0200

    Update TimeFormat.java

--- TimeFormat.java
+++ TimeFormat.java
@@ -5,7 +5,6 @@ public class TimeFormat {
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
 		// Does the same with the minutes part of the input.
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-		System.out.println(hours + " " + minutes);
 		if (hours<12){
 			System.out.println(hours + ":" + minutes + " AM");
 		}
